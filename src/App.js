f6b8e66420d06198fc38db2b716684edc13cbdd6
Dzhamal Chapanov <dzhamal.chapanov@gmail.com>
2023-01-07 12:27:18 +0700

    Clean Up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 import {useEffect} from "react";
-
-const tg = window.Telegram.WebApp;
+import Header from "./components/Header/Header";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready()
     }, [])
 
-    const onClose = () => {
-        tg.close()
-    }
 
   return (
     <div className="App">
-      work
-        <button onClick={onClose}>Закрыть</button>
+      <Header/>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
