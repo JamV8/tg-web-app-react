1da2439e6949a5dc0562a3577950fb1969f87e66
Dzhamal Chapanov <dzhamal.chapanov@gmail.com>
2023-01-10 13:33:47 +0700

    Form + Button params test

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import {useEffect} from "react";
 import Header from "./components/Header/Header";
 import {useTelegram} from "./hooks/useTelegram";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+    const {tg} = useTelegram();
 
     useEffect(() => {
         tg.ready()
@@ -14,7 +17,10 @@ function App() {
   return (
     <div className="App">
         <Header/>
-        <button onClick={onToggleButton}>toggle</button>
+        <Routes>
+            <Route index element={<ProductList/>}/>
+            <Route path={'form'} element={<Form/>}/>
+        </Routes>
     </div>
   );
 }
