2e9a60886fba38081b650ba9e2acf631ea052c6b
Dzhamal Chapanov <dzhamal.chapanov@gmail.com>
2023-01-17 12:58:49 -0800

    onSendData fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,7 +18,7 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data))
-    }, [])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
